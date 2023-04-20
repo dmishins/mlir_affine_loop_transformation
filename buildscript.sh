177a37e2a16a276a6a082475f7a0d2cc4070993b
LighghtEeloo <litiaeeloo@gmail.com>
2023-04-20 00:33:10 -0400

    build on site

--- buildscript.sh
+++ buildscript.sh
@@ -1,23 +1,21 @@
-echo "./bin/cgeist ${1}.c --raise-scf-to-affine -S  > ${1}.mlir"
-./bin/cgeist ${1}.c --raise-scf-to-affine -S  > ${1}.mlir
+POLY_PATH=../Polygeist/build/bin
+
+# echo "${POLY_PATH}/cgeist ${1}.c --raise-scf-to-affine -S > ${1}.mlir"
+${POLY_PATH}/cgeist ${1}.c --raise-scf-to-affine -S > ${1}.mlir
 
 
-echo "./bin/mlir-opt \
-    --lower-affine \
-    --convert-arith-to-llvm \
-    --convert-scf-to-cf \
-    --convert-func-to-llvm \
-    --reconcile-unrealized-casts ${1}.mlir | ./bin/mlir-translate --mlir-to-llvmir > ${1}.ll"
-./bin/mlir-opt -lower-affine -convert-scf-to-cf -memref-expand -convert-memref-to-llvm -convert-vector-to-llvm -convert-func-to-llvm -convert-cf-to-llvm -reconcile-unrealized-casts  ${1}.mlir | ./bin/mlir-translate --mlir-to-llvmir > ${1}.ll
+${POLY_PATH}/mlir-opt -lower-affine \
+    -convert-scf-to-cf \
+    -memref-expand \
+    -convert-memref-to-llvm \
+    -convert-vector-to-llvm \
+    -convert-func-to-llvm \
+    -convert-cf-to-llvm \
+    -reconcile-unrealized-casts \
+    ${1}.mlir | ${POLY_PATH}/mlir-translate --mlir-to-llvmir > ${1}.ll
 
 
-# ./bin/mlir-opt \
-#     --lower-affine \
-#     --convert-arith-to-llvm \
-#     --convert-scf-to-cf \
-#     --convert-func-to-llvm \
-#     --reconcile-unrealized-casts ${1}.mlir | ./bin/mlir-translate --mlir-to-llvmir > ${1}.ll
+# echo "${POLY_PATH}/clang ${1}.ll -o ${1}.out"
+${POLY_PATH}/clang ${1}.ll -o ${1}.out
 
-
-echo "./bin/clang ${1}.ll -o ${1}.out"
-./bin/clang ${1}.ll -o ${1}.out
+./${1}.out
