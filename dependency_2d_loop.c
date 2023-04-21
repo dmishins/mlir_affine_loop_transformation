95daa596c49b508f4b300d67d94269eb3e1bda45
LighghtEeloo <litiaeeloo@gmail.com>
2023-04-21 15:12:59 -0400

    format cases

--- dependency_2d_loop.c
+++ dependency_2d_loop.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-
 static int array[10000][1000];
 
-int main(){
-	int sum = 0;
-	for(size_t i = 0; i < 10000; i++){
-		array[i][0] = i;
-	}
-		for(size_t j = 1; j < 1000; j++){
-				for(size_t i = 0; i < 10000; i++){
+int main() {
+  int sum = 0;
+  for (size_t i = 0; i < 10000; i++) {
+    array[i][0] = i;
+  }
+  for (size_t j = 1; j < 1000; j++) {
+    for (size_t i = 0; i < 10000; i++) {
 
-			array[i][j] = array[i][j-1] + 1;
-		}
-	}
-	// for(int i = 0; i < 100000; i++){
-	// 	for(int j = 0; j < 1000; j++){
-	// 		//sum += array[i][j];
-	// 	}
-	// }
-	printf("%u\n", array[5][999]);
-	return sum;
+      array[i][j] = array[i][j - 1] + 1;
+    }
+  }
+  // for(int i = 0; i < 100000; i++){
+  // 	for(int j = 0; j < 1000; j++){
+  // 		//sum += array[i][j];
+  // 	}
+  // }
+  printf("%u\n", array[5][999]);
+  return sum;
 }
--- simple_1d_loop.c
+++ simple_1d_loop.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(){
-	int sum = 0;
-	for(int i = 0; i < 10; i++){
-		sum += i;
-		printf("%u  %u\n", i, sum);
-	}
-	return sum;
+int main() {
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += i;
+    printf("%u  %u\n", i, sum);
+  }
+  return sum;
 }
