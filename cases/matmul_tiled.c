ed05b1b8aadeb7b8b7faea1a0a1ef6522b4c25fe
Jihong Gan <jhgan99@gmail.com>
2023-04-23 16:55:13 -0400

    matmul example

---
+++ cases/matmul_tiled.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 10000
+#define M 10000
+#define P 10000
+#define TILE_SIZE 100
+
+void matmul(double **A, double **B, double **C, int n, int m, int p) {
+    for (int ii = 0; ii < n; ii += TILE_SIZE) {
+        for (int jj = 0; jj < p; jj += TILE_SIZE) {
+            for (int kk = 0; kk < m; kk += TILE_SIZE) {
+                for (int i = ii; i < ii + TILE_SIZE && i < n; i++) {
+                    for (int j = jj; j < jj + TILE_SIZE && j < p; j++) {
+                        double sum = 0;
+                        for (int k = kk; k < kk + TILE_SIZE && k < m; k++) {
+                            sum += A[i][k] * B[k][j];
+                        }
+                        C[i][j] += sum;
+                    }
+                }
+            }
+        }
+    }
+}
+
+double **allocate_matrix(int n, int m) {
+    double **matrix = (double **)malloc(n * sizeof(double *));
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (double *)malloc(m * sizeof(double));
+    }
+    return matrix;
+}
+
+void free_matrix(double **matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void fill_matrix(double **matrix, int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matrix[i][j] = (double)rand() / RAND_MAX;
+        }
+    }
+}
+
+int main() {
+    double **A = allocate_matrix(N, M);
+    double **B = allocate_matrix(M, P);
+    double **C = allocate_matrix(N, P);
+
+    srand(time(NULL));
+    fill_matrix(A, N, M);
+    fill_matrix(B, M, P);
+
+    matmul(A, B, C, N, M, P);
+
+    free_matrix(A, N);
+    free_matrix(B, M);
+    free_matrix(C, N);
+
+    return 0;
+}
