e33b9b36a8b9923ac8d764698db3be48384f51d8
LighghtEeloo <litiaeeloo@gmail.com>
2023-04-23 16:35:11 -0400

    sample test print

--- lib/polygeist/Passes/TestPrint.cpp
+++ lib/polygeist/Passes/TestPrint.cpp
@@ -23,23 +23,160 @@ using namespace mlir;
 using namespace mlir::scf;
 using namespace mlir::polygeist;
 
+// namespace {
+// /// This pass illustrates the IR nesting through printing.
+// struct TestPrintNestingPass
+//     : public PassWrapper<TestPrintNestingPass, OperationPass<>> {
+//   MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(TestPrintNestingPass)
+
+//   StringRef getArgument() const final { return "test-print-nesting"; }
+//   StringRef getDescription() const final { return "Test various printing."; }
+//   // Entry point for the pass.
+//   void runOnOperation() override {
+//     Operation *op = getOperation();
+//     resetIndent();
+//     printOperation(op);
+//   }
+
+//   /// The three methods below are mutually recursive and follow the nesting of
+//   /// the IR: operation->region->block->operation->...
+
+//   void printOperation(Operation *op) {
+//     // Print the operation itself and some of its properties
+//     printIndent() << "visiting op: '" << op->getName() << "' with "
+//                   << op->getNumOperands() << " operands and "
+//                   << op->getNumResults() << " results\n";
+//     // Print the operation attributes
+//     if (!op->getAttrs().empty()) {
+//       printIndent() << op->getAttrs().size() << " attributes:\n";
+//       for (NamedAttribute attr : op->getAttrs())
+//         printIndent() << " - '" << attr.getName().getValue() << "' : '"
+//                       << attr.getValue() << "'\n";
+//     }
+
+//     // Recurse into each of the regions attached to the operation.
+//     printIndent() << " " << op->getNumRegions() << " nested regions:\n";
+//     auto indent = pushIndent();
+//     for (Region &region : op->getRegions())
+//       printRegion(region);
+//   }
+
+//   void printRegion(Region &region) {
+//     // A region does not hold anything by itself other than a list of blocks.
+//     printIndent() << "Region with " << region.getBlocks().size()
+//                   << " blocks:\n";
+//     auto indent = pushIndent();
+//     for (Block &block : region.getBlocks())
+//       printBlock(block);
+//   }
+
+//   void printBlock(Block &block) {
+//     // Print the block intrinsics properties (basically: argument list)
+//     printIndent()
+//         << "Block with " << block.getNumArguments() << " arguments, "
+//         << block.getNumSuccessors()
+//         << " successors, and "
+//         // Note, this `.size()` is traversing a linked-list and is O(n).
+//         << block.getOperations().size() << " operations\n";
+
+//     // Block main role is to hold a list of Operations: let's recurse.
+//     auto indent = pushIndent();
+//     for (Operation &op : block.getOperations())
+//       printOperation(&op);
+//   }
+
+//   /// Manages the indentation as we traverse the IR nesting.
+//   int indent;
+//   struct IdentRAII {
+//     int &indent;
+//     IdentRAII(int &indent) : indent(indent) {}
+//     ~IdentRAII() { --indent; }
+//   };
+//   void resetIndent() { indent = 0; }
+//   IdentRAII pushIndent() { return IdentRAII(++indent); }
+
+//   llvm::raw_ostream &printIndent() {
+//     for (int i = 0; i < indent; ++i)
+//       llvm::outs() << "  ";
+//     return llvm::outs();
+//   }
+// };
+// } // namespace
 
 namespace {
 struct TestPrint : public TestPrintBase<TestPrint> {
   void runOnOperation() override {
-    // auto *parentOp = getOperation();
-    // MLIRContext *ctx = parentOp->getContext();
-    // RewritePatternSet patterns(ctx);
-    // patterns.add<ForBreakLoweringPattern>(patterns.getContext(), /*benefit=*/3);
-    // (void)applyPatternsAndFoldGreedily(parentOp, std::move(patterns));
+    Operation *op = getOperation();
+    resetIndent();
+    printOperation(op);
+  }
+
+  // Note: copy-paste from TestPrintNestingPass
+
+  /// The three methods below are mutually recursive and follow the nesting of
+  /// the IR: operation->region->block->operation->...
+
+  void printOperation(Operation *op) {
+    // Print the operation itself and some of its properties
+    printIndent() << "visiting op: '" << op->getName() << "' with "
+                  << op->getNumOperands() << " operands and "
+                  << op->getNumResults() << " results\n";
+    // Print the operation attributes
+    if (!op->getAttrs().empty()) {
+      printIndent() << op->getAttrs().size() << " attributes:\n";
+      for (NamedAttribute attr : op->getAttrs())
+        printIndent() << " - '" << attr.getName().getValue() << "' : '"
+                      << attr.getValue() << "'\n";
+    }
+
+    // Recurse into each of the regions attached to the operation.
+    printIndent() << " " << op->getNumRegions() << " nested regions:\n";
+    auto indent = pushIndent();
+    for (Region &region : op->getRegions())
+      printRegion(region);
+  }
+
+  void printRegion(Region &region) {
+    // A region does not hold anything by itself other than a list of blocks.
+    printIndent() << "Region with " << region.getBlocks().size()
+                  << " blocks:\n";
+    auto indent = pushIndent();
+    for (Block &block : region.getBlocks())
+      printBlock(block);
+  }
+
+  void printBlock(Block &block) {
+    // Print the block intrinsics properties (basically: argument list)
+    printIndent()
+        << "Block with " << block.getNumArguments() << " arguments, "
+        << block.getNumSuccessors()
+        << " successors, and "
+        // Note, this `.size()` is traversing a linked-list and is O(n).
+        << block.getOperations().size() << " operations\n";
+
+    // Block main role is to hold a list of Operations: let's recurse.
+    auto indent = pushIndent();
+    for (Operation &op : block.getOperations())
+      printOperation(&op);
+  }
+
+  /// Manages the indentation as we traverse the IR nesting.
+  int indent;
+  struct IdentRAII {
+    int &indent;
+    IdentRAII(int &indent) : indent(indent) {}
+    ~IdentRAII() { --indent; }
+  };
+  void resetIndent() { indent = 0; }
+  IdentRAII pushIndent() { return IdentRAII(++indent); }
+
+  llvm::raw_ostream &printIndent() {
+    for (int i = 0; i < indent; ++i)
+      llvm::outs() << "  ";
+    return llvm::outs();
   }
 };
 } // namespace
-
-// void mlir::polygeist::populateForBreakToWhilePatterns(
-//     RewritePatternSet &patterns) {
-//   patterns.add<ForBreakLoweringPattern>(patterns.getContext(), /*benefit=*/3);
-// }
 
 std::unique_ptr<Pass> mlir::polygeist::testPrint() {
   return std::make_unique<TestPrint>();
